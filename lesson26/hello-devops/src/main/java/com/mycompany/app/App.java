82bb25309304ff76eea66ed46fa47b4043b84f8d
goodpunk <andre3000@yandex.ru>
2025-04-15 15:32:29 +0000

    fix devops

--- lesson26/hello-devops/src/main/java/com/mycompany/app/App.java
+++ lesson26/hello-devops/src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello DevOps!!!!!";
+    private static final String MESSAGE = "Hello DevOps and DevOps!!!!!";
 
     public App() {}
 
